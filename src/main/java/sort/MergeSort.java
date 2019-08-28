992ba9224ffa5342c6e43ffe6c0fdc65d9a88968
Kaustubh <kkaustubh@us.ibm.com>
2019-08-28 06:48:06 -0700

    Implemented merge sort and one example of recursion to add all numbers from list.

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -2,7 +2,107 @@ package sort;
 
 public class MergeSort {
 
-    public void sort() {
-        System.out.println("Sort elements...");
+    public static int additionOfElementsWithRecursion(int[] elements, int beginIndex, int endIndex) {
+        if (beginIndex == endIndex) {
+            return elements[endIndex];
+        }
+
+        return elements[beginIndex] + additionOfElementsWithRecursion(elements, beginIndex + 1, endIndex);
     }
+
+    public static void mergeSort(int[] listToSort) {
+        System.out.println("----------- listToSort -----------");
+        for (int element : listToSort) {
+            System.out.print(" | " + element);
+        }
+        System.out.println(" |");
+
+        if (listToSort.length == 1) {
+            return;
+        }
+
+        int middleIndex = listToSort.length / 2 + listToSort.length % 2;
+        int[] lhList = new int[middleIndex];
+        int[] rhList = new int[listToSort.length - middleIndex];
+
+        split(listToSort, lhList, rhList);
+
+        mergeSort(lhList);
+        mergeSort(rhList);
+
+        merge(listToSort, lhList, rhList);
+    }
+
+    public static void split(int[] listToSort, int[] lhList, int[] rhList) {
+        for (int i = 0; i < listToSort.length; i++) {
+            if (i < lhList.length) {
+                lhList[i] = listToSort[i];
+            } else {
+                rhList[i - lhList.length] = listToSort[i];
+            }
+        }
+    }
+
+    public static void merge(int[] listToSort, int[] lhList, int[] rhList) {
+        System.out.println("----------- merge -----------");
+        System.out.println("------- ## LH List ## -------");
+        for (int element : lhList) {
+            System.out.print(" | " + element);
+        }
+        System.out.println(" |");
+        System.out.println("------- ## RH List ## -------");
+        for (int element : rhList) {
+            System.out.print(" | " + element);
+        }
+        System.out.println(" |");
+
+        if (listToSort.length == 1) {
+            return;
+        }
+
+        int lhPosition = 0, rhPosition = 0, sortedIndex = 0;
+
+        for (int i = 0; i < lhList.length; i++) {
+            if (rhPosition != rhList.length) {
+                for (int j = rhPosition; j < rhList.length; j++) {
+                    if (rhList[j] < lhList[i]) {
+                        listToSort[sortedIndex] = rhList[j];
+                        rhPosition = j + 1;
+                        sortedIndex++;
+                    } else {
+                        listToSort[sortedIndex] = lhList[i];
+                        lhPosition = i + 1;
+                        sortedIndex++;
+                        break;
+                    }
+                }
+            } else {
+                listToSort[sortedIndex] = lhList[i];
+                lhPosition = i + 1;
+                sortedIndex++;
+            }
+        }
+
+        if (lhPosition != lhList.length) {
+            for (int i = lhPosition; i < lhList.length; i++) {
+                listToSort[sortedIndex] = lhList[i];
+                sortedIndex++;
+            }
+        }
+
+        if (rhPosition != rhList.length) {
+            for (int i = rhPosition; i < rhList.length; i++) {
+                listToSort[sortedIndex] = rhList[i];
+                sortedIndex++;
+            }
+        }
+
+        System.out.println("---- [after merge] listToSort -------");
+        for (int element : listToSort) {
+            System.out.print(" | " + element);
+        }
+        System.out.println(" |");
+
+    }
+
 }
--- src/test/java/sort/MergeSortTest.java
+++ src/test/java/sort/MergeSortTest.java
@@ -2,12 +2,82 @@ package sort;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MergeSortTest {
 
     @Test
-    void sort() {
-        fail();
+    void additionOfElementsWithRecursion() {
+        //Given
+        int[] elements = {10, 1, 5, 8, 3};
+
+        //When
+        int total = MergeSort.additionOfElementsWithRecursion(elements, 0, elements.length - 1);
+
+        //Then
+        assertEquals(27, total);
     }
+
+    @Test
+    void additionOfElementsWithRecursionBetweenNumbers() {
+        //Given
+        int[] elements = {10, 1, 5, 8, 3};
+
+        //When
+        int total = MergeSort.additionOfElementsWithRecursion(elements, 2, elements.length - 1);
+
+        //Then
+        assertEquals(16, total);
+    }
+
+    @Test
+    void mergeSortSingleElement() {
+        //Given
+        int[] elements = {29};
+
+        //When
+        MergeSort.mergeSort(elements);
+
+        //Then
+        assertArrayEquals(new int[]{29}, elements);
+    }
+
+    @Test
+    void mergeSortTwoElements() {
+        //Given
+        int[] elements = {29, 18};
+
+        //When
+        MergeSort.mergeSort(elements);
+
+        //Then
+        assertArrayEquals(new int[]{18, 29}, elements);
+    }
+
+    @Test
+    void mergeSortExample1() {
+        //Given
+        int[] elements = {10, 1, 5, 8, 3};
+
+        //When
+        MergeSort.mergeSort(elements);
+
+        //Then
+        assertArrayEquals(new int[]{1, 3, 5, 8, 10}, elements);
+    }
+
+    @Test
+    void mergeSortExample2() {
+        //Given
+        int[] elements = {3, 5, 6, 8, 10, 1, 2, 4, 7, 9};
+
+        //When
+        MergeSort.mergeSort(elements);
+
+        //Then
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, elements);
+    }
+
+
 }
