9dd42fdc8cec676347054cdec2a38aaa6454ceb6
Kaustubh <kaustubh.kesarkar@gmail.com>
2020-10-07 06:56:14 +0530

    Added remove, print methods to binary search tree.

--- src/main/java/practicaldatastructure/datastructure/binaryseachtree/App.java
+++ src/main/java/practicaldatastructure/datastructure/binaryseachtree/App.java
@@ -3,6 +3,76 @@ package practicaldatastructure.datastructure.binaryseachtree;
 public class App {
 
     public static void main(String[] args) {
+
+        example3();
+        //example2();
+        //example1();
+
+    }
+
+    private static void example3() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(38, "38");
+        binarySearchTree.insert(26, "26");
+        binarySearchTree.insert(72, "72");
+        binarySearchTree.insert(55, "55");
+        binarySearchTree.insert(41, "41");
+        binarySearchTree.insert(60, "60");
+        binarySearchTree.insert(43, "43");
+        binarySearchTree.insert(90, "90");
+        binarySearchTree.insert(78, "78");
+        binarySearchTree.insert(74, "74");
+        binarySearchTree.insert(92, "92");
+
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+        System.out.println("Min: " + binarySearchTree.findMin());
+
+        System.out.println("===============");
+        binarySearchTree.remove(38);
+        binarySearchTree.remove(26);
+        System.out.println("After remove 38 and 26");
+        System.out.println("===============");
+        System.out.println("Root node is: " + (binarySearchTree.rootNode == null ? "null" : binarySearchTree.rootNode.key));
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+        System.out.println("Min: " + binarySearchTree.findMin());
+    }
+
+    private static void example2() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(40, "forty");
+
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
+        System.out.println("===============");
+        binarySearchTree.remove(40);
+        System.out.println("After remove 40");
+        System.out.println("===============");
+        System.out.println("Root node is: " + (binarySearchTree.rootNode == null ? "null" : binarySearchTree.rootNode.key));
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
+        binarySearchTree.insert(10, "ten");
+        binarySearchTree.insert(100, "hundred");
+        binarySearchTree.insert(70, "seventy");
+        binarySearchTree.insert(500, "hundred");
+        binarySearchTree.insert(40, "forty");
+
+        System.out.println("===============");
+        System.out.println("Root node is: " + (binarySearchTree.rootNode == null ? "null" : binarySearchTree.rootNode.key));
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
+        System.out.println("===============");
+        binarySearchTree.remove(10);
+        System.out.println("After remove 10");
+        System.out.println("===============");
+        System.out.println("Root node is: " + (binarySearchTree.rootNode == null ? "null" : binarySearchTree.rootNode.key));
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+    }
+
+    private static void example1() {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         binarySearchTree.insert(40, "forty");
         binarySearchTree.insert(30, "thirty");
@@ -10,16 +80,44 @@ public class App {
         binarySearchTree.insert(100, "hundred");
         binarySearchTree.insert(70, "seventy");
 
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
         System.out.println("Min: " + binarySearchTree.findMin());
         System.out.println("Max: " + binarySearchTree.findMax());
 
         binarySearchTree.insert(200, "two hundred");
-        System.out.println("Max: " + binarySearchTree.findMax());
+        System.out.println("Max after insert 200: " + binarySearchTree.findMax());
+
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
 
         int searchKey = 100;
         Node node = binarySearchTree.findNode(binarySearchTree.rootNode, searchKey);
         System.out.println(node == null ? "Node not found for key: " + searchKey : "Node found: " + node);
-
         System.out.println("Key " + searchKey + " node is leaf node: " + binarySearchTree.isLeafNode(node));
+
+
+        System.out.println("Removed " + 40 + ": " + binarySearchTree.remove(40));
+        node = binarySearchTree.findNode(binarySearchTree.rootNode, 40);
+        System.out.println(node == null ? "After remove: Node not found for key: " + 40 : "After remove: Node found: " + node);
+
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
+        System.out.println("===============");
+        binarySearchTree.insert(40, "forty");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
+
+        System.out.println("===============");
+        binarySearchTree.remove(200);
+        System.out.println("After remove 200");
+        System.out.println("===============");
+        System.out.println("Root node is: " + binarySearchTree.rootNode.key);
+        binarySearchTree.printBinarySearchTree(binarySearchTree.rootNode, "ROOT", 1);
     }
 }
--- src/main/java/practicaldatastructure/datastructure/binaryseachtree/BinarySearchTree.java
+++ src/main/java/practicaldatastructure/datastructure/binaryseachtree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package practicaldatastructure.datastructure.binaryseachtree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BinarySearchTree {
 
     public Node rootNode;
@@ -13,7 +16,7 @@ public class BinarySearchTree {
             Node currentNode = rootNode;
             while (true) {
                 parentNode = currentNode;
-                if (key <= parentNode.key) {
+                if (key < parentNode.key) {
                     currentNode = parentNode.lhNode;
                     if (currentNode == null) {
                         parentNode.lhNode = new Node(key, value);
@@ -31,7 +34,11 @@ public class BinarySearchTree {
     }
 
     public Node findMin() {
-        Node toReturn = rootNode;
+        return findMinFromNode(rootNode);
+    }
+
+    private Node findMinFromNode(Node node) {
+        Node toReturn = node;
 
         while (toReturn != null && toReturn.lhNode != null) {
             toReturn = toReturn.lhNode;
@@ -72,5 +79,117 @@ public class BinarySearchTree {
 
     public boolean isLeafNode(Node node) {
         return node == null || (node.lhNode == null && node.rhNode == null);
+    }
+
+    public boolean remove(int key) {
+        Node currentNode = rootNode;
+        Node parentNode = null;
+
+        boolean isLHNode = false;
+
+        while (currentNode != null) {
+            if (currentNode.key == key) {
+                break;
+            } else {
+                parentNode = currentNode;
+                if (currentNode.key > key) {
+                    currentNode = currentNode.lhNode;
+                    isLHNode = true;
+                } else {
+                    currentNode = currentNode.rhNode;
+                    isLHNode = false;
+                }
+            }
+        }
+
+        if (currentNode == null) {
+            return false;
+        } else if (parentNode == null) {
+            if (rootNode.rhNode != null) {
+                rootNode = getReplacedNodeWithLinking(rootNode);
+            } else {
+                rootNode = rootNode.lhNode;
+            }
+            return true;
+        } else {
+            if (isLeafNode(currentNode)) {
+                if (isLHNode) {
+                    parentNode.lhNode = null;
+                } else {
+                    parentNode.rhNode = null;
+                }
+            } else if (currentNode.rhNode == null) {
+                if (isLHNode) {
+                    parentNode.lhNode = currentNode.lhNode;
+                } else {
+                    parentNode.rhNode = currentNode.lhNode;
+                }
+            } else if (currentNode.lhNode == null) {
+                if (isLHNode) {
+                    parentNode.lhNode = currentNode.rhNode;
+                } else {
+                    parentNode.rhNode = currentNode.rhNode;
+                }
+            } else {
+                Node nodeToReplace = getReplacedNodeWithLinking(currentNode);
+                if (isLHNode) {
+                    parentNode.lhNode = nodeToReplace;
+                } else {
+                    parentNode.rhNode = nodeToReplace;
+                }
+            }
+            return true;
+        }
+    }
+
+    private Node getReplacedNodeWithLinking(Node node) {
+        Node findMinToReplace = findMinFromNode(node.rhNode);
+        if (findMinToReplace != null) {
+            remove(findMinToReplace.key);
+            findMinToReplace.lhNode = node.lhNode;
+            findMinToReplace.rhNode = node.rhNode;
+        }
+        return findMinToReplace;
+    }
+
+    private Map<Integer, String> printTreeLevelMap;
+
+    public void printBinarySearchTree(Node node, String type, int currentLevel) {
+        if (type.equals("ROOT")) {
+            printTreeLevelMap = new HashMap<>();
+        }
+        if (node == null) {
+            System.out.println("Empty tree!");
+            return;
+        }
+
+        if (node.lhNode != null) {
+            printBinarySearchTree(node.lhNode, "LH", currentLevel + 1);
+        } else {
+            addNodeValueToPrintMap(currentLevel + 1, "-");
+        }
+
+        if (node.rhNode != null) {
+            printBinarySearchTree(node.rhNode, "RH", currentLevel + 1);
+        } else {
+            addNodeValueToPrintMap(currentLevel + 1, "-");
+        }
+
+        addNodeValueToPrintMap(currentLevel, "" + node.key);
+
+        if (type.equals("ROOT")) {
+            for (String printLine : printTreeLevelMap.values()) {
+                System.out.println(printLine);
+            }
+        }
+
+    }
+
+    private void addNodeValueToPrintMap(int currentLevel, String value) {
+        if (printTreeLevelMap.get(currentLevel) == null) {
+            printTreeLevelMap.put(currentLevel, "(" + value + ")");
+        } else {
+            printTreeLevelMap.put(currentLevel, printTreeLevelMap.get(currentLevel) + " (" + value + ")");
+        }
     }
 }
